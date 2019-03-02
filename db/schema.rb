f12bab396ec2859c944989fd83666af4753f7855
Will Fischer <wfische0@chicagobooth.edu>
2019-03-02 19:01:11 +0000

    course index and show showing department

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180515221328) do
+ActiveRecord::Schema.define(version: 20190302183338) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180515221328) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "department_id"
   end
 
   create_table "departments", force: :cascade do |t|
