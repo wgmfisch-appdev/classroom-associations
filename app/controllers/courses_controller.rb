5a6fa2219dff889cde48cb372c2b1ec6c9f6518c
Will Fischer <wfische0@chicagobooth.edu>
2019-03-02 20:34:09 +0000

    with courses listing in departments 50 percent through

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -19,6 +19,7 @@ class CoursesController < ApplicationController
     @course = Course.new
 
     @course.title = params.fetch("title")
+    @course.department_id = params.fetch("department_id")
 
     if @course.valid?
       @course.save
@@ -39,6 +40,7 @@ class CoursesController < ApplicationController
     @course = Course.find(params.fetch("id_to_modify"))
 
     @course.title = params.fetch("title")
+    @course.department_id = params.fetch("department_id")
 
     if @course.valid?
       @course.save
--- app/models/department.rb
+++ app/models/department.rb
@@ -9,4 +9,5 @@
 #
 
 class Department < ApplicationRecord
+  
 end
